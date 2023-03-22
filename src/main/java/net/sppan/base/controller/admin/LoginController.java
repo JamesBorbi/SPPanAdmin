355ea20073cdce9ac78b62029dde8158134d4cd2
zbd <zhengbingdong@shein.com>
2023-03-22 20:35:48 +0800

    200322：页面

--- src/main/java/net/sppan/base/controller/admin/LoginController.java
+++ src/main/java/net/sppan/base/controller/admin/LoginController.java
@@ -2,18 +2,28 @@ package net.sppan.base.controller.admin;
 
 import net.sppan.base.controller.BaseController;
 
+import net.sppan.base.entity.User;
+import net.sppan.base.service.IUserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController extends BaseController {
+
+	@Autowired
+	private IUserService userService;
+
+
+
 	@RequestMapping(value = { "/admin/login" }, method = RequestMethod.GET)
 	public String login() {
 
@@ -27,7 +37,14 @@ public class LoginController extends BaseController {
 			 Subject subject = SecurityUtils.getSubject();
 			 UsernamePasswordToken token = new UsernamePasswordToken(username, password);
 			subject.login(token);
-			return redirect("/admin/index");
+
+			//登陆成功后跳转
+			User user = userService.findByUserName(username);
+			if(!ObjectUtils.nullSafeEquals(1,user.getId())){
+				return redirect("/back/index.html");
+			}else {
+				return redirect("/admin/index");
+			}
 		} catch (AuthenticationException e) {
 			model.put("message", e.getMessage());
 		}
--- src/main/java/net/sppan/base/controller/back/BackController.java
+++ src/main/java/net/sppan/base/controller/back/BackController.java
@@ -1,20 +1,24 @@
 package net.sppan.base.controller.back;
 
 import net.sppan.base.controller.BaseController;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BackController extends BaseController{
 	@RequestMapping(value ={"/back"})
 	public String config(){
-		
+
 		return "/back/config.html";
 	}
-	
-//	@RequestMapping(value = {"/admin/welcome"})
-//	public String welcome(){
-//
-//		return "admin/welcome";
-//	}
+
+	@RequestMapping(value = { "/back/logout" }, method = RequestMethod.GET)
+	public String logout() {
+		Subject subject = SecurityUtils.getSubject();
+		subject.logout();
+		return redirect("/admin/login");
+	}
 }
