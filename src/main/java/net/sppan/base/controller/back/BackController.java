940b1a2fad087c8cd3fecf255d112c1b143e8aa0
musk_zheng <111@qq.com>
2023-03-24 00:22:33 +0800

    网站网页整理

--- src/main/java/net/sppan/base/controller/back/BackController.java
+++ src/main/java/net/sppan/base/controller/back/BackController.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BackController extends BaseController{
-	@RequestMapping(value ={"/back"})
-	public String config(){
-
-		return "/back/config.html";
-	}
 
 	@RequestMapping(value = { "/back/logout" }, method = RequestMethod.GET)
 	public String logout() {
